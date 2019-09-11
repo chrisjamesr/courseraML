aabd63fb66224db3f2b04c1b97710534fadac891
chrisjamesr <chrisjames.r@gmail.com>
2019-09-11 13:10:06 -0400

    Add completed correct cost function and regularization penalty

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -18,8 +18,15 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+% Xbias = [ones(m,1),X];
+penalty = lambda/(2*m)*sum(theta.^2,2);
 
+J = 1/(2*m) * sum( (X*theta- y) .^2 ) + penalty;
 
+Xgrad = [zeros(m,1),X];
+gradPenalty = theta*(lambda/m);
+
+grad = (1/m) * (X*theta - y)'*X + gradPenalty';
 
 
 
