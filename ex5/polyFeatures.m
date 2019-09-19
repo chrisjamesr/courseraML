0fc8927564564070d8018adeafde413c042051a7
chrisjamesr <chrisjames.r@gmail.com>
2019-09-19 13:05:46 -0400

    Create matrix of polynomial features from X

--- ex5/polyFeatures.m
+++ ex5/polyFeatures.m
@@ -14,10 +14,14 @@ X_poly = zeros(numel(X), p);
 %               column of X contains the values of X to the p-th power.
 %
 % 
+% X_poly(1) = X(1)
+% for i = 2:p
+%   X_poly(i) = [X_poly(i-1,), X(i).^i, X_poly(:,i+1:end)];
+% end
 
-
-
-
+for i = 1:p
+    X_poly(:,i) = X.^i;
+end
 
 
 % =========================================================================
