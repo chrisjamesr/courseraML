6e12d8f194ba0ff99f4e2303f8927e8943f5331c
chrisjamesr <chrisjames.r@gmail.com>
2019-10-09 17:46:04 -0400

    Calculate correct cost and gradient

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -38,20 +38,17 @@ Theta_grad = zeros(size(Theta));
 %                 partial derivatives w.r.t. to each element of X
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
-%
+% 
 
+penalty = (lambda/2)*sum(sum(Theta.^2)) + (lambda/2)*sum(sum(X.^2));
 
+J = sum(sum((((X*Theta')' - Y ).* R).^2 ).* R) ) /2 + penalty;
+X_grad = ((R.*((X*Theta')' - Y ))* Theta) + (lambda*X);
+Theta_grad = ((R.*((X*Theta')' - Y ))'* X) + (lambda*Theta );
 
-
-
-
-
-
-
-
-
-
-
+% J = sum(sum((((X*Theta') - Y).*R).^2))/2 + (lambda/2)*sum(sum(Theta.^2)) + (lambda/2)*sum(sum(X.^2));
+% X_grad = (((X*Theta') - Y).*R) * Theta  + lambda*X;
+% Theta_grad = (((X*Theta') - Y).*R)' * X + lambda*Theta;
 
 
 
